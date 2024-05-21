6c1db51c66c70941bdfd58edf29536dcad8c0204
Rjack00 <rjackson77@comcast.net>
2024-05-21 17:25:40 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+function getRandomComputerResult() {
+    const options = ["Rock", "Paper", "Scissors"];
+    return options[Math.floor(Math.random() * 3)];
+  }
+  console.log(getRandomComputerResult());
